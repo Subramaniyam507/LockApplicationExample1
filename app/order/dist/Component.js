dc8778c6c92c06801363c49d7021cb4065147af6
Subramaniyam N <subramaniyam96@gmail.com>
2024-02-25 13:14:34 +0000

    sync

--- app/order/dist/Component.js
+++ app/order/dist/Component.js
@@ -1,1 +1,1 @@
-sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("name.order.Component",{metadata:{manifest:"json"}})});
+sap.ui.define(["sap/fe/core/AppComponent"],function(e){"use strict";return e.extend("namspace.order.Component",{metadata:{manifest:"json"}})});
